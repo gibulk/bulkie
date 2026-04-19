da50ba1aeeb90ffedb9eaea2904177e25fd992c3
G!bulk <gibulk@yahoo.com>
2026-04-19 17:29:24 +0700

    Create robots.js

---
+++ app/robots.js
@@ -0,0 +1,12 @@
+export default function robots() {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://bulkie.vercel.app'
+  
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/api/'],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
