94ce0e0e314c68897b4e7253e5c349b80d1846d2
G!bulk <gibulk@yahoo.com>
2026-04-19 16:51:41 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['bulkie.vercel.app', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+}
 
+module.exports = nextConfig
